ebc172ce41daf31744edd2b34d5e6d00996f0e9a
Jonas Fischer <fijo.com@googlemail.com>
2015-09-17 23:05:34 +0200

    added database scripts add .gitignore

---
+++ dbscripts/ECP/2015-9-17_1-Fijo-Add-List-Indexes.sql
@@ -0,0 +1,8 @@
+ALTER TABLE fittingruleentity ADD INDEX (lastModified);
+ALTER TABLE fittingruleentity ADD INDEX (userId, lastModified);
+
+ALTER TABLE compositionentity ADD INDEX (lastModified);
+ALTER TABLE compositionentity ADD INDEX (userId, lastModified);
+
+ALTER TABLE rulesetentity ADD INDEX (lastModified);
+ALTER TABLE rulesetentity ADD INDEX (userId, lastModified);
