0b73f580798a49d94c17ee9800f1c8f2f9acf91d
Jonas Fischer <fijo.com@googlemail.com>
2015-09-28 18:07:29 +0200

    corrected path

--- install-requirements-ubuntu.sh
+++ install-requirements-ubuntu.sh
@@ -9,11 +9,11 @@ sudo service apache2 reload
 sudo apt-get install nodejs
 sudo apt-get install npm
 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF -y
 echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
-sudo apt-get update
+sudo apt-get update -q
 sudo apt-get install mono-complete
 
 cd ~
 sudo curl -sS https://getcomposer.org/installer| php
-sudo mv composer.phar /usr/local/bin/composer
+sudo mv composer.phar /usr/bin/composer
